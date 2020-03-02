74943434dec72287f51d61104d6f24e79707089b
Jake <finalblade1234@gmail.com>
2020-03-01 18:14:33 -0700

    Stats panel event. Only works for general tab. (blocks with docks is included by default

---
+++ insert/api/listener/helpers/StructureTab.java
@@ -0,0 +1,18 @@
+package api.listener.helpers;
+
+/**
+ * All of the advanced build mode structure tabs.
+ */
+public enum StructureTab {
+    STATS,
+    DOCKS,
+    FACTION,
+    GENERAL,
+    GUISGROUP, //wtf is this
+    HELP_TOP,
+    POWER,
+    SHIELD,
+    STRUCTURE,
+    THRUSTER,
+    WEAPONS
+}
