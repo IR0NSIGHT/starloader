74b0316944a2ff09d22ce43cdadc70864da2c927
TheDerpGamer <garret2kool@gmail.com>
2020-03-19 09:31:56 -0700

    Merge branch 'master' of https://gitlab.com/generic-username/starloader
    # Conflicts:
    #	insert/api/listener/events/TargetPanelDrawEvent.java
    #	insert/api/listener/helpers/StructureTab.java

---
+++ insert/api/inventory/InventoryType.java
@@ -0,0 +1,12 @@
+package api.inventory;
+
+public enum InventoryType {
+    PLAYER_INVENTORY,
+    PLAYER_INVENTORY_REFINERY,
+    PLAYER_INVENTORY_FACTORY,
+    CREATIVE,
+    CREATIVE_SHIPYARD,
+    STORAGE_BOX,
+    FACTORY,
+    SHIPYARD
+}
