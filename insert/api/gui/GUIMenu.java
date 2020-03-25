5bad9548c6b6f19917ce1b89816c51137235af71
TheDerpGamer <garret2kool@gmail.com>
2020-03-25 09:58:26 -0700

    Merge branch 'master' of https://gitlab.com/generic-username/starloader
    # Conflicts:
    #	insert/api/listener/events/TargetPanelDrawEvent.java
    #	insert/api/listener/helpers/StructureTab.java

--- insert/api/gui/GUIMenu.java
+++ insert/api/gui/GUIMenu.java
@@ -2,31 +2,9 @@ package api.gui;
 
 public class GUIMenu {
 
-    private char hotKey;
-    private boolean hasHotKey;
-    private String name;
-    private int[] relativePos = new int[2];
-    private int[] preferredSize = new int[2];
-    private int[] minSize = new int[2];
-    private int[] maxSize = new int[2];
+    private String displayName;
 
-    public GUIMenu(String name, int[] relativePos, int[] preferredSize, int[] minSize, int[] maxSize) {
-        this.name = name;
-        this.relativePos = relativePos;
-        this.preferredSize = preferredSize;
-        this.minSize = minSize;
-        this.maxSize = maxSize;
-        this.hasHotKey = false;
+    public GUIMenu(String displayName) {
+        this.displayName = displayName;
     }
-
-    public GUIMenu(String name, int[] relativePos, int[] preferredSize, int[] minSize, int[] maxSize, char hotKey) {
-        this.name = name;
-        this.relativePos = relativePos;
-        this.preferredSize = preferredSize;
-        this.minSize = minSize;
-        this.maxSize = maxSize;
-        this.hotKey = hotKey;
-        this.hasHotKey = true;
-    }
-
 }
