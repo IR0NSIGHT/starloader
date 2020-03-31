75623fdaab5a2739c3a02fcf1da709057d815eef
Jake <jakevvolkenburg@gmail.com>
2020-03-31 10:59:36 -0600

    Entity merge

--- insert/api/entity/Entity.java
+++ insert/api/entity/Entity.java
@@ -5,28 +5,21 @@ import api.systems.Reactor;
 import api.systems.Shield;
 import api.universe.Universe;
 import org.schema.game.common.controller.SegmentController;
+import org.schema.game.common.controller.elements.ManagerContainer;
+import org.schema.game.common.controller.elements.ShieldLocal;
+import org.schema.game.common.controller.elements.ShipManagerContainer;
+import org.schema.game.common.controller.elements.SpaceStationManagerContainer;
+import org.schema.game.common.data.ManagedSegmentController;
 import org.schema.game.common.data.world.SimpleTransformableSendableObject;
 import org.schema.schine.graphicsengine.core.GlUtil;
 import javax.vecmath.Vector3f;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Entity {
     public SegmentController internalEntity;
-    private List<Ship> dockedEntities;
-    private double speed;
-    private double maxSpeed;
-    private boolean hasReactor;
-    private List<Reactor> reactors;
-    private Reactor activeReactor;
-    private boolean hasShields;
-    private List<Shield> shields;
-    private String name;
-    private Faction faction;
-    private Universe universe;
-    private EntityType entityType;
-
-
 
     public Entity(SegmentController controller) {
         internalEntity = controller;
@@ -125,22 +118,46 @@ public class Entity {
     public void playEffect(byte value){
         internalEntity.executeGraphicalEffectServer(value);
     }
+    public ArrayList<Ship> getDockedEntities(){
+        ArrayList<SegmentController> collection = new ArrayList<>();
+        internalEntity.railController.getDockedRecusive(collection);
+        ArrayList<Ship> ships = new ArrayList<>();
+        for (SegmentController controller : collection){
+            if(controller instanceof org.schema.game.common.controller.Ship){
+                ships.add(new Ship((org.schema.game.common.controller.Ship) controller));
+            }
+        }
+        return ships;
+    }
+    public ArrayList<Shield> getShields(){
+        ManagerContainer<?> manager = getManagerContainer();
+        ArrayList<Shield> shields = new ArrayList<>();
+        if(manager instanceof ShipManagerContainer){
+            Collection<ShieldLocal> allShields = ((ShipManagerContainer) manager).getShieldAddOn().getShieldLocalAddOn().getAllShields();
+            for (ShieldLocal sh : allShields){
+                shields.add(new Shield(sh));
+            }
+            return shields;
+        }else if(manager instanceof SpaceStationManagerContainer){
+            //TODO implement other types of shields
+        }else{
+            //no shields
+        }
+        return shields;
+    }
+    private ManagerContainer<?> getManagerContainer(){
+        return ((ManagedSegmentController<?>) internalEntity).getManagerContainer();
+    }
 
     public EntityType getEntityType() {
-        //(For some reason a Switch statement won't work here, I tried
         EntityType entityType = null;
-        if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.SPACE_STATION) {
-            entityType = EntityType.STATION;
-        } else if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.SHIP) {
-            entityType = EntityType.SHIP;
-        } else if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.ASTEROID) {
-            entityType = EntityType.ASTEROID;
-        } else if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.PLANET_CORE) {
-            entityType = EntityType.PLANETCORE;
-        } else if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.PLANET_SEGMENT) {
-            entityType = EntityType.PLANETSEGMENT;
-        } else if(internalEntity.getType() == SimpleTransformableSendableObject.EntityType.SHOP) {
-            entityType = EntityType.SHOP;
+        switch(internalEntity.getType()){
+            case SPACE_STATION: entityType = EntityType.STATION; break;
+            case SHIP: entityType = EntityType.SHIP; break;
+            case ASTEROID: entityType = EntityType.ASTEROID; break;
+            case PLANET_CORE: entityType = EntityType.PLANETCORE; break;
+            case PLANET_SEGMENT: entityType = EntityType.PLANETSEGMENT; break;
+            case SHOP: entityType = EntityType.SHOP; break;
         }
         return entityType;
     }
