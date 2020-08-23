63393a68d5f1843a363417da0ee60a7a8b8aff9b
jake <jakevvolkenburg@gmail.com>
2020-08-23 11:13:22 -0700

    Merged EM/CM utils from star-api into starloader

--- insert/api/utils/game/SegmentControllerUtils.java
+++ insert/api/utils/game/SegmentControllerUtils.java
@@ -1,9 +1,11 @@
 package api.utils.game;
 
 import api.ModPlayground;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.shorts.ShortArrayList;
 import org.schema.game.common.controller.ManagedUsableSegmentController;
 import org.schema.game.common.controller.SegmentController;
+import org.schema.game.common.controller.elements.*;
 import org.schema.game.common.controller.elements.power.reactor.tree.ReactorElement;
 import org.schema.game.common.controller.elements.power.reactor.tree.ReactorTree;
 import org.schema.game.common.data.element.ElementInformation;
@@ -19,9 +21,11 @@ public class SegmentControllerUtils {
     /**
      * Gets a Vector3f of the entity's current direction.
      */
+    @NotNull
     public static Vector3f getDirection(SegmentController controller) {
         return GlUtil.getForwardVector(new Vector3f(), controller.getWorldTransform());
     }
+
     @Nullable
     public static ReactorElement getChamberFromElement(@NotNull ManagedUsableSegmentController<?> controller, @NotNull ElementInformation blockId){
         for (ReactorElement child : getAllChambers(controller)) {
@@ -48,5 +52,53 @@ public class SegmentControllerUtils {
         }
         //Add self
         arr.add(element);
+    }
+
+    @NotNull
+    public static ObjectArrayList<ManagerModule<?, ?, ?>> getManagerModules(ManagedUsableSegmentController<?> ent){
+        return ent.getManagerContainer().getModules();
+    }
+
+    @Nullable
+    public static <EM extends UsableElementManager<?, ?, ?>> EM getElementManager(ManagedUsableSegmentController<?> ent, Class<EM> classType){
+        for (ManagerModule<?, ?, ?> managerModule : getManagerModules(ent)) {
+            UsableElementManager<?, ?, ?> elementManager = managerModule.getElementManager();
+            if(elementManager.getClass().equals(classType)){
+                return (EM) elementManager;
+            }
+        }
+        return null;
+    }
+    public <CM extends ElementCollectionManager> ArrayList<ElementCollectionManager> getCollectionManagers(ManagedUsableSegmentController<?> ent, Class<CM> classType){
+        ArrayList<ElementCollectionManager> ecms = new ArrayList<ElementCollectionManager>();
+        for (ManagerModule<?, ?, ?> module : ent.getManagerContainer().getModules()) {
+            if(module instanceof ManagerModuleCollection){
+                for (Object cm : ((ManagerModuleCollection) module).getCollectionManagers()) {
+                    if(cm.getClass().equals(classType)) {
+                        ecms.add((ElementCollectionManager) cm);
+                    }
+                }
+            }else if(module instanceof ManagerModuleSingle){
+                ElementCollectionManager cm = ((ManagerModuleSingle) module).getCollectionManager();
+                if(cm.getClass().equals(classType)){
+                    ecms.add(cm);
+                }
+            }//else{ something broke }
+        }
+        return ecms;
+    }
+    public ArrayList<ElementCollectionManager> getAllCollectionManagers(ManagedUsableSegmentController<?> ent){
+        ArrayList<ElementCollectionManager> ecms = new ArrayList<ElementCollectionManager>();
+        for (ManagerModule<?, ?, ?> module : ent.getManagerContainer().getModules()) {
+            if(module instanceof ManagerModuleCollection){
+                for (Object cm : ((ManagerModuleCollection) module).getCollectionManagers()) {
+                    ecms.add((ElementCollectionManager) cm);
+                }
+            }else if(module instanceof ManagerModuleSingle){
+                ElementCollectionManager cm = ((ManagerModuleSingle) module).getCollectionManager();
+                ecms.add(cm);
+            }
+        }
+        return ecms;
     }
 }
